e3a040aa5d91a7f6a06b38662d67af233cdf8393
mrfatihasci <126919157+mrfatihasci@users.noreply.github.com>
2023-11-14 05:01:55 +0300

    edit .js to manipulate DOM, edit .html &css

--- javascript.js
+++ javascript.js
@@ -4,12 +4,14 @@ let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
 
-function getComputerChoice() {
+
+function getComputerChoice() {   //return computerSelection as string of rock/paper/scissor
    let selectionArray = ['rock', 'paper', 'scissor'];
    return selectionArray[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
+
+function playRound(playerSelection, computerSelection) {   // return none, increment round, increment winners score
    if (playerSelection == computerSelection) {
       ++round;
    }
@@ -27,14 +29,85 @@ function playRound(playerSelection, computerSelection) {
    }
 }
 
+
+const playerNode = document.querySelector('.player');
+const playerP = document.querySelector('.playerP');
+const computerNode =document.querySelector('.computer');
+const computerP =document.querySelector('.computerP');
+function updateScoreContainer(){       // finding ratio depending on the current score
+   playerNode.setAttribute('style',`flex-grow: ${(playerScore+0.005)/(computerScore + playerScore + 0.01)};`);
+   playerP.innerHTML =`YOU: ${playerScore}--`;
+   computerNode.setAttribute('style',`flex-grow:${(computerScore+0.005)/(computerScore + playerScore + 0.01)} ;`);
+   computerP.innerHTML=`--${computerScore} COMPUTER`;
+}
+
+const container = document.querySelector('#button-container');
+const body=document.querySelector('body');
+const pSelection=document.createElement('p');
+pSelection.setAttribute('style','color: white; text-align: center; font-size:22px;');
+body.appendChild(pSelection);
+const pScore =document.createElement('p');
+pScore.setAttribute('style','color: white; text-align: center; font-size:22px;');
+body.appendChild(pScore);
+const pResult=document.createElement('p');
+pResult.setAttribute('style','color: white; text-align: center; font-size:26px;');
+body.appendChild(pResult);
+
+
+container.addEventListener('click', (event)=>{
+   playerSelection=`${event.target.textContent}`;
+   computerSelection=getComputerChoice();
+   playRound(playerSelection, computerSelection);
+   updateScoreContainer();
+   pSelection.textContent=`${playerSelection} ---------  ${computerSelection}`;
+   pScore.textContent=`${playerScore}-${computerScore}`;
+   if ((playerScore == 5)||(computerScore==5)) {
+      if (playerScore == 5){
+         pResult.textContent=`You Win! ${playerScore}---${computerScore}`;
+         computerScore=0;
+         playerScore=0;
+      }
+      else{
+         pResult.textContent=`Computer Win! ${playerScore}---${computerScore}`;
+         computerScore=0;
+         playerScore=0;
+      }
+   }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const one = document.getElementById('one');
 const two = document.getElementById('two');
+
+
 for (let i = 0; i <= 4; i++) {
    playerSelection = prompt("type rock/paper/scissor");
+
    computerSelection = getComputerChoice();
+
    playRound(playerSelection, computerSelection);
+
    console.log(`PlayervsComputer(${playerSelection}-${computerSelection}) @round${round}: computer:${computerScore} user:${playerScore}`);
-   if (i == 4) {
+  
+  
+   if (round == 5) {
       if (playerScore >> computerScore) {
          console.log(`You Win`);
          one.innerHTML = `You Win`;
@@ -52,7 +125,5 @@ for (let i = 0; i <= 4; i++) {
       }
    }
 }
-
-
-
+*/
 
